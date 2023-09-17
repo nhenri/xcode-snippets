282d00ebb7bacf197d600ff9f9da024a0cd2412e
Nicolas HENRI <nicolas.henri@radiofrance.com>
2023-09-17 21:30:56 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -8,15 +8,17 @@ if [ ! -f "$PWD"/install.sh ]; then
     exit 1
 fi
 
-if [ -d "$LIBRARY_SNIPPETS_DIR" ] && [ ! -L "$LIBRARY_SNIPPETS_DIR" ]; then
-   echo "'$LIBRARY_SNIPPETS_DIR' found and now copying files, please wait ..."
-   cp "$LIBRARY_SNIPPETS_DIR"/*.codesnippet ./CodeSnippets
-   echo "Backup done !"
-   echo "Remove directory '$LIBRARY_SNIPPETS_DIR', please wait ..."
-   rm -Rf "$LIBRARY_SNIPPETS_DIR"
-else
-    echo "'$LIBRARY_SNIPPETS_DIR' found but it's symbolic link"
-    exit 1
+if [ -d "$LIBRARY_SNIPPETS_DIR" ]; then
+    if [ ! -L "$LIBRARY_SNIPPETS_DIR" ]; then
+        echo "'$LIBRARY_SNIPPETS_DIR' found and now copying files, please wait ..."
+        cp "$LIBRARY_SNIPPETS_DIR"/*.codesnippet ./CodeSnippets
+        echo "Backup done !"
+        echo "Remove directory '$LIBRARY_SNIPPETS_DIR', please wait ..."
+        rm -Rf "$LIBRARY_SNIPPETS_DIR"
+    else
+        echo "'$LIBRARY_SNIPPETS_DIR' found but it's symbolic link"
+        exit 1
+    fi
 fi
 
 ln -s "$PWD"/CodeSnippets "$LIBRARY_SNIPPETS_DIR"
