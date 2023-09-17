885b1682662c4532b892c8dc74835c2b0902e9f9
Nicolas HENRI <nicolas.henri@radiofrance.com>
2023-09-17 15:14:13 +0200

    Setup repo and add my codesnippets

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# xcode-snippets
-Xcode snippets backup-import
+# Xcode snippets backup-import & Cheatsheet
+
+Xcode snippets backup-import with symbolic link
+
+### Installation
+
+**Fork the repository,** to be able to backup and import your own snippets. You can save the repository wherever you want.
+
+```bash
+$ git git@github.com:nhenri/xcode-snippets.git
+$ cd xcode-snippets
+$ ./install.sh
+```
+
+## Cheat Sheet
+
+* // Copyright © Radio France. All rights reserved. - `RF_Copy`
+* // MARK: - `RF_Mark`
+* // TODO: NHE - `RF_Todo`
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+PWD=$(pwd)
+LIBRARY_SNIPPETS_DIR="$HOME/Library/Developer/Xcode/UserData/CodeSnippets"
+
+if [ ! -f "$PWD"/install.sh ]; then 
+    echo "Error: run install.sh in git directory xcode-snippets"
+    exit 1
+fi
+
+if [ -d "$LIBRARY_SNIPPETS_DIR" ] && [ ! -L "$LIBRARY_SNIPPETS_DIR" ]; then
+   echo "'$LIBRARY_SNIPPETS_DIR' found and now copying files, please wait ..."
+   cp "$LIBRARY_SNIPPETS_DIR"/*.codesnippet ./CodeSnippets
+   echo "Backup done !"
+   echo "Remove directory '$LIBRARY_SNIPPETS_DIR', please wait ..."
+   rm -Rf "$LIBRARY_SNIPPETS_DIR"
+else
+    echo "'$LIBRARY_SNIPPETS_DIR' found but it's symbolic link"
+    exit 1
+fi
+
+ln -s "$PWD"/CodeSnippets "$LIBRARY_SNIPPETS_DIR"
+
+echo "Install completed! 🚀"
+echo "Restart Xcode"
+
